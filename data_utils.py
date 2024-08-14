f81cc94d67335b3c5b1440f551fc8c363ca292b1
jungjaewon <woodcook486@naver.com>
2024-08-14 23:45:27 +0900

    add data_utils.py add wandb

---
+++ data_utils.py
@@ -0,0 +1,82 @@
+import logging
+import os
+import torch
+import os.path as osp
+import random, pickle
+
+from PIL import Image
+from glob import glob
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler, Dataset
+
+
+logger = logging.getLogger(__name__)
+
+class Classification_2d(Dataset):
+
+    def __init__(self, transform, mode):
+        self.image_dir = osp.join('outfitdata_set3_4598', mode)
+        self.transform = transform
+        self.mode = mode
+
+        self.outfit_list = glob(osp.join(self.image_dir, '*'))
+        self.outfit_data = list()
+        random.shuffle(self.outfit_list)
+
+        with open('outfitdata_set3_tagged.plk', 'rb') as fp:
+            self.tagged_dict = pickle.load(fp)[mode]
+
+        for outfit_id_path in self.outfit_list:
+            outfit_id = outfit_id_path.split(os.sep)[-1]
+            for i in range(1, 6):
+                data_path = osp.join(outfit_id_path, f'{i}.jpg')
+                cat_idx = self.tagged_dict[f'{outfit_id}_{str(i)}']['cate_idx']
+                self.outfit_data.append([data_path, cat_idx])
+
+    def __getitem__(self, index):
+        data = self.outfit_data[index]
+        img_path = data[0]
+        cate_idx = data[1]
+        target_image = Image.open(osp.join(img_path))
+        target_image = target_image.convert('RGB')
+        return self.transform(target_image), cate_idx#torch.LongTensor([cate_idx])
+
+    def __len__(self):
+        return len(self.outfit_data)
+
+def get_loader(args):
+    if args.local_rank not in [-1, 0]:
+        torch.distributed.barrier()
+
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+    transform_test = transforms.Compose([
+        transforms.Resize((args.img_size, args.img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+    if args.dataset == 'polyvore':
+        trainset = Classification_2d(transform_train, 'train')
+        testset = Classification_2d(transform_test, 'test')
+
+    if args.local_rank == 0:
+        torch.distributed.barrier()
+
+    train_sampler = RandomSampler(trainset) if args.local_rank == -1 else DistributedSampler(trainset)
+    test_sampler = SequentialSampler(testset)
+    train_loader = DataLoader(trainset,
+                              sampler=train_sampler,
+                              batch_size=args.train_batch_size,
+                              num_workers=4,
+                              pin_memory=True)
+    test_loader = DataLoader(testset,
+                             sampler=test_sampler,
+                             batch_size=args.eval_batch_size,
+                             num_workers=4,
+                             pin_memory=True) if testset is not None else None
+
+    return train_loader, test_loader
--- mae_pretrain.py
+++ mae_pretrain.py
@@ -3,9 +3,14 @@ import argparse
 import math
 import torch
 import torchvision
+import wandb
+import cv2
+wandb.require("core")
+
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import ToTensor, Compose, Normalize
 from tqdm import tqdm
+from data_utils import get_loader
 
 from model import *
 from utils import setup_seed
@@ -21,8 +26,14 @@ if __name__ == '__main__':
     parser.add_argument('--total_epoch', type=int, default=2000)
     parser.add_argument('--warmup_epoch', type=int, default=200)
     parser.add_argument('--model_path', type=str, default='vit-t-mae.pt')
+    parser.add_argument('--dataset', type=str, default='polyvore')
+    parser.add_argument('--img_size', type=int, default=256)
+    parser.add_argument('--local_rank', type=int, default=-1)
 
     args = parser.parse_args()
+
+    wandb.login(key='3b3fd7ec86b8f3f0f32f2d7a78456686d8755d99')
+    wandb.init(project=f'mae_{args.dataset}', name=args.name)
 
     setup_seed(args.seed)
 
@@ -32,10 +43,16 @@ if __name__ == '__main__':
     assert batch_size % load_batch_size == 0
     steps_per_update = batch_size // load_batch_size
 
-    train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-    val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-    dataloader = torch.utils.data.DataLoader(train_dataset, load_batch_size, shuffle=True, num_workers=4)
-    writer = SummaryWriter(os.path.join('logs', 'cifar10', 'mae-pretrain'))
+    train_dir = f'{args.dataset}_train'
+    os.makedirs(train_dir, exist_ok=True)
+
+    if args.dataset == 'cifar10':
+        train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
+        val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
+        dataloader = torch.utils.data.DataLoader(train_dataset, load_batch_size, shuffle=True, num_workers=4)
+    elif args.dataset == 'polyvore':
+        train_loader, val_loader = get_loader(args)
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     model = MAE_ViT(mask_ratio=args.mask_ratio).to(device)
@@ -48,7 +65,7 @@ if __name__ == '__main__':
     for e in range(args.total_epoch):
         model.train()
         losses = []
-        for img, label in tqdm(iter(dataloader)):
+        for img, label in tqdm(iter(train_dataset)):
             step_count += 1
             img = img.to(device)
             predicted_img, mask = model(img)
@@ -60,11 +77,12 @@ if __name__ == '__main__':
             losses.append(loss.item())
         lr_scheduler.step()
         avg_loss = sum(losses) / len(losses)
-        writer.add_scalar('mae_loss', avg_loss, global_step=e)
+        wandb.log({'train_loss': avg_loss})
         print(f'In epoch {e}, average traning loss is {avg_loss}.')
 
         ''' visualize the first 16 predicted images on val dataset'''
         model.eval()
+        cnt = 0
         with torch.no_grad():
             val_img = torch.stack([val_dataset[i][0] for i in range(16)])
             val_img = val_img.to(device)
@@ -72,7 +90,9 @@ if __name__ == '__main__':
             predicted_val_img = predicted_val_img * mask + val_img * (1 - mask)
             img = torch.cat([val_img * (1 - mask), predicted_val_img, val_img], dim=0)
             img = rearrange(img, '(v h1 w1) c h w -> c (h1 h) (w1 v w)', w1=2, v=3)
-            writer.add_image('mae_image', (img + 1) / 2, global_step=e)
-        
+            img = (img + 1) / 2
+            img = img.detach().numpy()
+            cv2.imwrite(os.path.join(train_dir, f'val_{cnt}.jpg'), img)
+
         ''' save model '''
-        torch.save(model, args.model_path)
+        torch.save(model, os.path.join(train_dir, args.model_path))
